0459ca5e14a9bb6477e8b144055abc82f0997517
eeTiboah <etiboah@gmail.com>
2022-05-22 23:09:08 +0000

    add modal

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
 
 
 const accordion = document.getElementsByClassName('accordion')
+function accordionfn(){
+  for (let i=0; i<accordion.length; i++){
+    accordion[i].addEventListener('click', function(){
+      this.classList.toggle('active');
+      let accordion_data = this.nextElementSibling;
 
-for (let i=0; i<accordion.length; i++){
-  accordion[i].addEventListener('click', function(){
-    this.classList.toggle('active');
-    let accordion_data = this.nextElementSibling;
-
-    if (accordion_data.style.display === "flex"){
-      accordion_data.style.display = "none";
-    } else {
-      accordion_data.style.display = "flex";
-    }
-  })
+      if (accordion_data.style.display === "flex"){
+        accordion_data.style.display = "none";
+      } else {
+        accordion_data.style.display = "flex";
+      }
+    })
+  }
 }
 
-const firstOption = document.getElementsByClassName("preference")
+accordionfn()
 
-for (let option of firstOption){
-  option.addEventListener('click', function(){
-    for (let option of firstOption){
-      option.classList.remove('active_color')
-    }
-    option.classList.add('active_color')
-    const preference = option.children[0].innerHTML
-    console.log(preference)
-    document.getElementById('answer').innerHTML = preference
-  })
+const preferences = document.getElementsByClassName("preference")
+const beanType = document.getElementsByClassName("bean")
+const quantity = document.getElementsByClassName("quantity")
+const grindOption = document.getElementsByClassName("grind")
+const deliveries = document.getElementsByClassName("deliver")
+
+function orderSummaryData(classNameList, elementIdNameOne, elementIdNameTwo){
+  for (let option of classNameList){
+    option.addEventListener('click', function(){
+      for (let option of classNameList){
+        option.classList.remove('active_color')
+      }
+        option.classList.add('active_color')
+        const value = option.children[0].innerHTML
+        document.getElementById(elementIdNameOne).innerHTML = value
+        document.getElementById(elementIdNameTwo).innerHTML = value
+  
+    })
+  }
 }
+
+orderSummaryData(preferences, 'answer1', 'modal1')
+orderSummaryData(beanType, 'answer2', 'modal2')
+orderSummaryData(quantity, 'answer3', 'modal3')
+orderSummaryData(grindOption, 'answer4', 'modal4')
+orderSummaryData(deliveries, 'answer5', 'modal5')
+
+const modal = document.getElementById('modal')
+const modalBtn = document.getElementById("modalbtn")
+
+modalBtn.onclick = function(){
+  modal.style.display = 'block'
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
